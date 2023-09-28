0b4a7647dc4ddcb3948d73145b4b34900a5617ed
Alexandra Shevchenko <alexshevchenko95@gmail.com>
2023-09-28 12:55:25 +0300

    stage 1

---
+++ ToyRaffle.java
@@ -0,0 +1,8 @@
+public class ToyRaffle {
+  public static void main(String[] args) {
+    String inpuString = "1 constuctor 2; 2 doll 2; 3 robot 6" ;
+    String[] toysArr = inpuString.split(';');
+    
+
+  }
+}
